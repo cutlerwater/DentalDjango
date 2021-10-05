7c5f67bd62d5f493d4803008891e73613fa6ad68
cutlerwater <cutlerwater2@live.com>
2021-10-04 23:42:02 -0400

    third-commit

--- newdentistwebsite/settings.py
+++ newdentistwebsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import django_heroku
 import dj_database_url 
-from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
